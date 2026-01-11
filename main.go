07e100504eeb6d33efba33b40006770ea748d427
beatricepang <s3956978@student.rmit.edu.au>
2026-01-11 16:53:46 +1100

    initialised album structure, then create an album slice  and created a gin.Context.

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// INITIALISING THE STRUCTURE
+type album struct {
+	ID     string  `json: "id"`
+	Title  string  `json:"title"`
+	Artist string  `json: "artist"`
+	Price  float64 `json: "price"`
+}
+
+// album slice to seed record to album data
+
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vuaghan", Price: 39.99},
+}
+
+// creates JSON from the slice of album structs
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+
+	router.Run("localhost:8080")
+}
