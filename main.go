10b65309ca3dfe45674e1b1df6a546170902e7dc
beatricepang <s3956978@student.rmit.edu.au>
2026-01-11 17:16:18 +1100

    added router GET method to main

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func getAlbums(c *gin.Context) {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 	router.Run("localhost:8080")
 }
